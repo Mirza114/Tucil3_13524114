48a48cf108bd617d900fc2361555b54953f804d1
Mirza TW <13524114@std.stei.itb.ac.id>
2026-05-10 23:01:02 +0700

    Refactor SearchResult class to simplify structure
    Removed unused fields and constructor from SearchResult class.

--- src/SearchResult.java
+++ src/SearchResult.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.ArrayList;
 
 public class SearchResult {
@@ -16,22 +15,3 @@ public class SearchResult {
         this.time = time;
     }
 }
-=======
-import java.util.List;
-
-public class SearchResult {
-    List<String> moves;
-    List<State> states;
-    int totalCost;
-    int iterations;
-    long executionTime;
-
-    public SearchResult(List<String> moves, List<State> states, int totalCost, int iterations, long executionTime) {
-        this.moves = moves;
-        this.states = states;
-        this.totalCost = totalCost;
-        this.iterations = iterations;
-        this.executionTime = executionTime;
-    }
-}
->>>>>>> 9ef2c15eb393d366f5acec4b095247fbde6a67ff
