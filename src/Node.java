db00b4246e320298ea3ae7728e6528c63821ddb5
Mirza TW <13524114@std.stei.itb.ac.id>
2026-05-10 23:00:38 +0700

    Resolve merge conflict in Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Node {
     State state;
     Node parent;
@@ -14,18 +13,3 @@ public class Node {
         this.priority = priority;
     }
 }
-=======
-public class Node {
-    State state;
-    Node parent;
-    String move;
-    int cost;
-
-    public Node(State state, Node parent, String move, int cost) {
-        this.state = state;
-        this.parent = parent;
-        this.move = move;
-        this.cost = cost;
-    }
-}
->>>>>>> 9ef2c15eb393d366f5acec4b095247fbde6a67ff
